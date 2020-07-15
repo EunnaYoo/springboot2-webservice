650e561c76ee05fc24def9dc5330df5e2f19237c
eunnaYoo <dmssk6788@gmail.com>
2020-07-15 17:49:45 +0900

    200715_JPA설정및API개발

---
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor // 기본 생성자 자동 추가 ex) public Posts() {}
+@Entity // 테이블과 링크될 클래스임을 나타냄
+public class Posts {
+
+    @Id // PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK생성규칙 : auto_increment
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
